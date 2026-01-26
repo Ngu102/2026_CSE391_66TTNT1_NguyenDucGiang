3bc5a7df0db8a6d9a45ba66e3136d260712d440d
Thu6ngay13 <Ngu102@example.com>
2026-01-26 23:25:00 +0700

    First commit - Portfolio

---
+++ 10 basic projects/Portfolio/script.js
@@ -0,0 +1,12 @@
+window.onload = function() { // sự kiện sảy ra khi toàn bộ trang (html+css) đã tải xong
+    alert("Quỳ xuống đi. Mệnh lệnh của tôi là tuyệt đối!"); // hiện thị thông báo
+};
+
+<script src="script.js"></script> // liên kết file script.js vào file html
+const menuLinks = document.querySelectorAll("nav ul li a"); // chọn tất cả các thẻ a trong menu
+menuLinks.forEach(link => { // forEach: duyệt qua từng link
+    link.addEventListener("click", function() { // gắn sự kiện click cho từng link
+        alert("Bạn vừa chọn mục: " + this.textContent); //this.textContent: lấy nội dung chữ của từng link
+
+    });
+});
