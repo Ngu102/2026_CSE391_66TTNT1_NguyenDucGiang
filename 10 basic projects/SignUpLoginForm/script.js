4302cace4cf5d80fcf51b95f2a1165abfa5bb374
Thu6ngay13 <Ngu102@example.com>
2026-01-30 09:07:59 +0700

    Add basic projects and update portfolio

---
+++ 10 basic projects/SignUpLoginForm/script.js
@@ -0,0 +1,45 @@
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('signupUsername').value;
+    const email = document.getElementById('signupEmail').value;
+    const password = document.getElementById('signupPassword').value;
+
+    if  (username.length < 3) {
+        alert("Username must be at least 3 characters long.");
+        return;
+    }
+    if (!validateEmail(email)) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        return;
+    }
+
+    alert("Sign Up Successful!");
+    this.reset();
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;   
+    if (!validateEmail(email)) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        return;
+    }
+    alert("Login Successful!");
+    this.reset();
+});
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
